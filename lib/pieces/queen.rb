28d5254c3488a5cf34440d9297a6d21532978484
Gavin OMelia <gavin@omelia.org>
2025-03-07 14:46:25 -0500

    Refactor queen to just combine rook and bishop move

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,30 +1,12 @@
+require_relative 'rook'
+require_relative 'bishop'
+
 class Queen < Piece
   def initialize(color)
     super(:queen, color)
   end
 
   def find_moves(position)
-    x, y = position
-    moves = []
-
-    # Horizontal moves
-    (0..7).each do |i|
-      moves << [x, i] unless i == y
-    end
-
-    # Vertical moves
-    (0..7).each do |i|
-      moves << [i, y] unless i == x
-    end
-
-    # Diagonal moves
-    (-7..7).each do |i|
-      next if i.zero?
-
-      moves << [x + i, y + i] if Board.on_board?(x + i, y + i)
-      moves << [x + i, y - i] if Board.on_board?(x + i, y - i)
-    end
-
-    moves
+    Rook.new(color).find_moves(position) + Bishop.new(color).find_moves(position)
   end
 end
